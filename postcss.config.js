df5f2fd934e30b01a00fdca4a110ca0676d5191e
Vignesh Mudhiraj <vigguvignesh92@gmail.com>
2024-05-16 22:50:02 +0530

    god

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
+import autoprefixer from "autoprefixer";
+import tailwindcss from "tailwindcss";
+
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [tailwindcss, autoprefixer],
 };
